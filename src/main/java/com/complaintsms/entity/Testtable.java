8c5c371c1bf47cc9a5d5ec58ccf282ff6c5608c5
sharonosullivan <sharonosullivan2@hotmail.com>
2017-03-12 19:25:47 +0000

    queries and test table

---
+++ src/main/java/com/complaintsms/entity/Testtable.java
@@ -0,0 +1,21 @@
+package com.complaintsms.entity;
+
+/**
+ * Created by Sharon on 12/03/2017.
+ */
+public class Testtable {
+
+    private String testcolumn;
+
+
+    public String getTestcol() {
+        return testcolumn;
+    }
+
+    public void setTestcol(String username) {
+        this.testcolumn = testcolumn;
+    }
+
+
+}
+
